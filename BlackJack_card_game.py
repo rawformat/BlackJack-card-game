a8ae00c5433fdf96bc87f0be02af767b390fb38b
Istillstrange <sanya123563sanya@gmail.com>
2022-04-07 20:04:58 +0300

    Create BlackJack_card_game.py

---
+++ BlackJack_card_game.py
@@ -0,0 +1,13 @@
+'''
+The Black Jack card game
+'''
+import random
+
+#CARDS
+suits = ('Hearts', 'Diamonds', 'Clubs', 'Spades')
+
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight',
+        'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
+        'Nine':9, 'Ten':10, 'Jack':11,'Queen':12, 'King':13, 'Ace':14}
