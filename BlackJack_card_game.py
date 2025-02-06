1670e20c6135715bd0e955307f029273a140c070
Alexander Tolstorebrov <sanya123563sanya@gmail.com>
2025-02-06 16:21:54 +0200

    Just a test of git

--- BlackJack_card_game.py
+++ BlackJack_card_game.py
@@ -3,7 +3,7 @@ The Black Jack card game
 '''
 import random
 
-#CARDS
+#CARDS 52
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
